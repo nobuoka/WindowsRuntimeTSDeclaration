c97ac10690d31044ef0e8b054a897bc54b3bef73
nobuoka <nobuoka@vividcode.info>
2015-07-05 02:50:06 +0900

    Add `Microsoft.CodeAnalysis.CSharp` to references

--- WindowsRuntimeTSDeclaration/Program.cs
+++ WindowsRuntimeTSDeclaration/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace WindowsRuntimeTSDeclaration
 {
@@ -10,6 +7,8 @@ namespace WindowsRuntimeTSDeclaration
     {
         static void Main(string[] args)
         {
+            var tree = CSharpSyntaxTree.ParseText("namespace HelloWorld {}");
+            var root = (CompilationUnitSyntax)tree.GetRoot();
         }
     }
 }
