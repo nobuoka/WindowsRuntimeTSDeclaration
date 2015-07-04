b5304438de71853f1174920b90809b909c764552
nobuoka <nobuoka@vividcode.info>
2015-07-05 03:47:11 +0900

    A `module` declaration can be generated

---
+++ WindowsRuntimeTSDeclaration.Test/TsDeclarationGeneratorTest.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WindowsRuntimeTSDeclaration.Test
+{
+    [TestClass]
+    public class TsDeclarationGeneratorTest
+    {
+        [TestMethod]
+        public void generateOnlyNamespaceWithSimpleNameDeclaration()
+        {
+            var generated = generateDeclaration("namespace HelloWorld {}");
+            Assert.AreEqual("module HelloWorld {\n}\n", generated);
+        }
+
+        [TestMethod]
+        public void generateOnlyNamespaceWithQualifiedNameDeclaration()
+        {
+            var generated = generateDeclaration("namespace HelloWorld.Qualified.Name {}");
+            Assert.AreEqual("module HelloWorld.Qualified.Name {\n}\n", generated);
+        }
+
+        private string generateDeclaration(string csStr)
+        {
+            var writer = new StringWriter();
+            try
+            {
+                var tree = CSharpSyntaxTree.ParseText(csStr);
+                var g = new TsDeclarationGenerator(tree, writer);
+                g.Generate();
+            }
+            finally
+            {
+                writer.Close();
+            }
+            return writer.ToString();
+        }
+    }
+}
---
+++ WindowsRuntimeTSDeclaration/TsDeclarationGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace WindowsRuntimeTSDeclaration
+{
+    public class TsDeclarationGenerator
+    {
+        private SyntaxTree tree;
+        private TextWriter writer;
+
+        public TsDeclarationGenerator(SyntaxTree tree, TextWriter writer)
+        {
+            this.tree = tree;
+            this.writer = writer;
+        }
+
+        public void Generate()
+        {
+            var root = (CompilationUnitSyntax)tree.GetRoot();
+            foreach (var memberDecl in root.Members)
+            {
+                procMemberDeclarationSyntax(memberDecl, Console.Out, 0);
+            }
+        }
+
+        private void procMemberDeclarationSyntax(MemberDeclarationSyntax syntax, TextWriter writer, int indentCount)
+        {
+            if (syntax is NamespaceDeclarationSyntax)
+            {
+                procNamespaceDeclarationSyntax((NamespaceDeclarationSyntax)syntax);
+            }
+        }
+
+        private void procNamespaceDeclarationSyntax(NamespaceDeclarationSyntax syntax)
+        {
+            writer.Write("module " + syntax.Name + " {\n");
+            writer.Write("}\n");
+        }
+    }
+}
